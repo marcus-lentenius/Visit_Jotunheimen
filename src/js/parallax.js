dae91f0ef23a12667a041554a64363db75ab0651
Marcus <m.lentenius@gmail.com>
2020-04-30 12:19:06 +0200

    changed images cleaned up code cleaned up features fixed footer

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -8,6 +8,5 @@ $(document).ready(function () {
         $('.bg_image_1').css('top', '+' + (scrolledY * 0.9) + 'px');
         $('.bg_image_2').css('top', '+' + (scrolledY * 0.1) - 130 + 'px');
         $('.bg_image_3').css('top', '+' + (scrolledY * 0.1) - 250 + 'px');
-        // $('.bg_image_4').css('top', '+' + (scrolledY * 0.1) - 500 + 'px');
     }
 });
