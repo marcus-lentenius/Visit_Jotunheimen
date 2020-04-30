c663233964ac77797d80d387df2242e1dfe92e94
Marcus <m.lentenius@gmail.com>
2020-04-30 21:24:06 +0200

    hiking updated to 1920x1080 resolution text added

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    var width = $(window).width();
+    if (width < 360) {
+        $(".right_text_wrapper").hide();
+        var text = $(".right_box_para").text();
+        $(".left_text_wrapper").append(text);
+    }
+
     $(window).bind('scroll', function (e) {
         parallaxScroll();
     });
@@ -9,4 +16,12 @@ $(document).ready(function () {
         $('.bg_image_2').css('top', '+' + (scrolledY * 0.1) - 130 + 'px');
         $('.bg_image_3').css('top', '+' + (scrolledY * 0.1) - 250 + 'px');
     }
+
+    // $("div").click(function () {
+    //     $('html, body').animate({
+    //         scrollTop: $("#lodging").offset().top
+    //     }, 1000);
+    // })
 });
+
+
